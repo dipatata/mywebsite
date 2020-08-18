2eef40ca3ab36ea2f9f8deabb13bfa5d17eb5edb
alejandro_barreto <aledbarreto@gmail.com>
2020-08-18 14:57:32 -0300

    Add route for blog

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const routes = {
     spaceshipR : ['/spaceship/:lan', '/spaceship' ],
     cvR : ['/cv/:lan', '/cv' ], 
     javaR : ['/java/:lan', '/java' ],
+    blogR : ['/blog/:lan', '/blog' ],
     homeR : '/'
 }
 
